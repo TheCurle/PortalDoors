dd291277dd778cc9a0409c6fe74278f186cca073
Curle <curle@gemwire.uk>
2021-05-03 03:21:12 +0100

    Did somebody say new project? smh.

---
+++ src/main/java/uk/gemwire/portaldoors/PortalDoors.java
@@ -0,0 +1,8 @@
+package uk.gemwire.portaldoors;
+
+import net.minecraftforge.fml.common.Mod;
+
+@Mod(PortalDoors.ID)
+public class PortalDoors {
+    public static final String ID = "portaldoors";
+}
